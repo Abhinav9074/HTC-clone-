84ad516815f485541aa91070c715a6b3305f6ed7
Abhinav9074 <abhinavp9074@gmail.com>
2023-07-29 15:54:57 +0530

    third commit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const mobile_nav = document.querySelector('.mobile-navbar-btn');
 const nav_header = document.querySelector('.header');
 const products = document.getElementById('products');
 const nav_drop_div = document.querySelector('.nav-drop-div');
+const header = document.querySelector('.header');
+const navbar_link = document.getElementsByClassName('navbar-link')
 
 mobile_nav.addEventListener('click', function(){
     nav_header.classList.toggle("active");
@@ -24,6 +26,20 @@ products.addEventListener('mouseover', function() {
 
 products.addEventListener('mouseout', function() {
     nav_drop_div.style.display = "none";
+})
+
+header.addEventListener('mouseover', function() {
+    header.style.backgroundColor = "#FFFFFF"
+    for(let i = 0; i < navbar_link.length; i++) {
+    navbar_link[i].style.color = "#656565";
+    }
+})
+
+header.addEventListener('mouseout', function() {
+    header.style.backgroundColor = "rgba(0, 0, 0, .5)"
+    for(let i = 0; i < navbar_link.length; i++) {
+    navbar_link[i].style.color = "#FFFFFF";
+    }
 })
 
 });
