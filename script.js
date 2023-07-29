d660b9e57f6ef63e4320b5bb672539fe7480ba14
Abhinav9074 <abhinavp9074@gmail.com>
2023-07-29 18:28:23 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,71 +1,85 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-const mobile_nav = document.querySelector('.mobile-navbar-btn');
-const nav_header = document.querySelector('.header');
-const products = document.getElementById('products');
-const nav_drop_div = document.querySelector('.nav-drop-div');
-const header = document.querySelector('.header');
-const navbar_link = document.getElementsByClassName('navbar-link')
-const icon = document.getElementsByClassName('search-icon')
-const search_icon = document.getElementById('searching-icon');
-const close_icon = document.getElementById('close-icon');
-const search_box = document.getElementById('searching-box');
+    const mobile_nav = document.querySelector('.mobile-navbar-btn');
+    const nav_header = document.querySelector('.header');
+    const products = document.getElementById('products');
+    const nav_drop_div = document.querySelector('.nav-drop-div');
+    const header = document.querySelector('.header');
+    const main_head = document.getElementsByClassName('header')[0]
+    const navbar_link = document.getElementsByClassName('navbar-link')
+    const icon = document.getElementsByClassName('search-icon')
+    const search_icon = document.getElementById('searching-icon');
+    const close_icon = document.getElementById('close-icon');
+    const search_box = document.getElementById('searching-box');
+    const tri_white = document.getElementById('tri-white');
+    var isOpen = 0
 
-mobile_nav.addEventListener('click', function(){
-    nav_header.classList.toggle("active");
-})
+    mobile_nav.addEventListener('click', function () {
+        nav_header.classList.toggle("active");
+    })
 
-products.addEventListener('click', function() {
-    const currentDisplayStyle = window.getComputedStyle(nav_drop_div).display;
-    if (currentDisplayStyle === "none") {
+    products.addEventListener('click', function () {
+        const currentDisplayStyle = window.getComputedStyle(nav_drop_div).display;
+        if (currentDisplayStyle === "none") {
+            nav_drop_div.style.display = "block";
+        } else {
+            nav_drop_div.style.display = "none";
+        }
+    });
+
+    products.addEventListener('mouseover', function () {
         nav_drop_div.style.display = "block";
-    } else {
+    })
+
+    products.addEventListener('mouseout', function () {
         nav_drop_div.style.display = "none";
-    }
-});
+    })
 
-products.addEventListener('mouseover', function() {
-    nav_drop_div.style.display = "block";
-})
-
-products.addEventListener('mouseout', function() {
-    nav_drop_div.style.display = "none";
-})
-
-header.addEventListener('mouseover', function() {
-    header.style.backgroundColor = "#FFFFFF"
-    for(let i = 0; i < navbar_link.length; i++) {
-    navbar_link[i].style.color = "#656565";
-    }
-
-    for(let j = 0; j < icon.length; j++) {
-        icon[j].style.color = "#656565";
+    header.addEventListener('mouseover', function () {
+        header.style.backgroundColor = "#FFFFFF"
+        tri_white.style.display = "none";
+        for (let i = 0; i < navbar_link.length; i++) {
+            navbar_link[i].style.color = "#656565";
         }
-})
 
-header.addEventListener('mouseout', function() {
-    header.style.backgroundColor = "rgba(0, 0, 0, .5)"
-    for(let i = 0; i < navbar_link.length; i++) {
-    navbar_link[i].style.color = "#FFFFFF";
-    }
-
-    for(let j = 0; j < icon.length; j++) {
-        icon[j].style.color = "#FFFFFF";
+        for (let j = 0; j < icon.length; j++) {
+            icon[j].style.color = "#656565";
         }
-})
 
-search_icon.addEventListener('click', function() {
+        
+    })
 
-    search_box.style.display = "block"
-    close_icon.style.display = "block"
+    header.addEventListener('mouseout', function () {
+        if(isOpen==0){
+            header.style.backgroundColor = "rgba(0, 0, 0, .5)"
+            tri_white.style.display = "block";
+        
+        for (let i = 0; i < navbar_link.length; i++) {
+            navbar_link[i].style.color = "#FFFFFF";
+        }
 
-})
+        for (let j = 0; j < icon.length; j++) {
+            icon[j].style.color = "#FFFFFF";
+        }
+    }
+    })
 
-close_icon.addEventListener('click', function() {
+    search_icon.addEventListener('click', function () {
+        search_box.style.display = "block"
+        close_icon.style.display = "block"
+        isOpen = 1
+        console.log(isOpen)
 
-    search_box.style.display = "none"
-    close_icon.style.display = "none"
-})
+    })
+
+    close_icon.addEventListener('click', function () {
+
+        search_box.value = ""
+        search_box.style.display = "none"
+        close_icon.style.display = "none"
+        isOpen = 0
+    })
+
 
 
 
